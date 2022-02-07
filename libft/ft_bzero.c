667b7cb6fee52a24c7bbbd820bb7f50c4432edf2
yazilimfuryasi <84662757+yazilimfuryasi@users.noreply.github.com>
2022-02-07 11:01:46 +0300

    Update ft_bzero.c

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -3,15 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ehazir <ehazir@42.tr>                      +#+  +:+       +#+        */
+/*   By: ehazir <eneshazrr@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/01 17:19:04 by ehazir            #+#    #+#             */
-/*   Updated: 2022/02/01 17:19:07 by ehazir           ###   ########.tr       */
+/*   Created: 2022/02/07 11:01:30 by ehazir            #+#    #+#             */
+/*   Updated: 2022/02/07 11:01:33 by ehazir           ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+/*
+Bellekte n kadar s baytındaki verileri siler. '\0' (null) alana kadar.
+*/
 void ft_bzero(void *s, size_t n)
 {
     size_t i;
@@ -26,3 +28,17 @@ void ft_bzero(void *s, size_t n)
     }
     s = byt;
 }
+
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[] = "eglendiremediklerimizdenmisiniz";
+    
+    ft_bzero(str, 15);
+    printf(": %s", str);
+    printf("\n: %s", bzero(str, 18));
+}
+*/

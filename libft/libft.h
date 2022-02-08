930f777905353015ce99304112538db4f95fc00c
yazilimfuryasi <84662757+yazilimfuryasi@users.noreply.github.com>
2022-02-08 12:31:21 +0300

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -33,9 +33,9 @@ void	*ft_memchr(const void *s, int c, size_t n);
 
 size_t	ft_strlen(const char *);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
-
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 #endif

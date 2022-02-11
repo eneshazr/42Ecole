c1d8329cb2120502313e34ac686421215baae9de
YAZILIM FURYASI <84662757+yazilimfuryasi@users.noreply.github.com>
2022-02-11 17:05:20 +0300

    Add files via upload

---
+++ libft/ft_substr.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ehazir <eneshazrr@gmail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/11 12:11:59 by ehazir            #+#    #+#             */
+/*   Updated: 2022/02/11 16:31:04 by ehazir           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	if ((size_t)start > ft_strlen(s))
+		return (ft_strdup(""));
+	str = malloc(sizeof(char) * (len + 1));
+	i = 0;
+	if (!str)
+		return (0);
+	while (i < len)
+	{
+		str[i] = *(s + start + i);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+
+#include <stdlib.h>
+#include <stdio.h>
+int main()
+{
+	char *str;
+
+	str = ft_substr("Ferdi TAYFUR", 6, 5);
+	printf("%s\n", str);
+}
